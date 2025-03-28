a43499c8743d3bb8a909eee1b80a40c400b2e0ed
kwikwik0603 <45453610+kwikwik0603@users.noreply.github.com>
2025-03-27 18:24:02 -0700

    noclue

--- Unity1_GD78_SatvikKaushik_a1/Assets/Script/S_PlayerMovement.cs
+++ Unity1_GD78_SatvikKaushik_a1/Assets/Script/S_PlayerMovement.cs
@@ -1,16 +1,44 @@
+using System;
 using UnityEngine;
 
 public class S_PlayerMovement : MonoBehaviour
 {
+
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private Vector3 moveDirection;
+    private Rigidbody rb;
+    [SerializeField] private float jumpForce;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(moveDirection * moveSpeed);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horInput = Input.GetAxis("Horizontal");
+        float verInput = Input.GetAxis("Vertical");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            DoJump();
+        }
+
+        moveDirection = new Vector3(horInput, 0, verInput);
+
+    }
+
+    void DoJump()
+    {
+        Vector3 force = new Vector3(0, jumpForce, 0);
+        rb.AddForce(force, ForceMode.Impulse);
     }
 }
